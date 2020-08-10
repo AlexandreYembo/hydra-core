408ab578a754b6b6ed9552be6cd61d8b6901b555
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-08-10 01:03:20 +0100

    Update IMediatorHandler.cs

--- Hydra.Core/Communication/Mediator/IMediatorHandler.cs
+++ Hydra.Core/Communication/Mediator/IMediatorHandler.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using Hydra.Core.Messages;
+using Hydra.Core.Messages.CommonMessages.DomainEvents;
 using Hydra.Core.Messages.CommonMessages.Notifications;
 
 namespace Hydra.Core.Communication.Mediator
@@ -7,6 +8,7 @@ namespace Hydra.Core.Communication.Mediator
     public interface IMediatorHandler
     {
         Task PublishEvent<T>(T tEvent) where T : Event;
+        Task PublishDomainEvent<T>(T tEvent) where T : DomainEvent;
         Task<bool> SendCommand<T>(T command) where T : Command;
         Task PublishNotification<T>(T notification) where T : DomainNotification;
     }
