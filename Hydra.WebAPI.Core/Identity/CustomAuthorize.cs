f6bcf915cd5a1eacec26f97e5e1a3da32f3a3024
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-10-03 11:12:05 +0100

    Create CustomAuthorize.cs

---
+++ Hydra.WebAPI.Core/Identity/CustomAuthorize.cs
@@ -0,0 +1,49 @@
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Hydra.WebAPI.Core.Identity
+{
+    /// <summary>
+    /// Valid claims
+    /// </summary>
+    public class CustomAuthorization
+    {
+        public static bool UserClaimsValid(HttpContext context, string claimName, string claimValue) =>
+            context.User.Identity.IsAuthenticated && context.User.Claims.Any(c => c.Type == claimName && c.Value.Contains(claimName));
+    }
+
+    public class ClaimsAuthorizeAttribute : TypeFilterAttribute
+    {
+        public ClaimsAuthorizeAttribute(string claimName, string claimValue) : base(typeof(ClaimFilter))
+        {
+            Arguments = new object[] { new Claim(claimName, claimValue) };
+        }
+    }
+
+    public class ClaimFilter : IAuthorizationFilter
+    {
+        private readonly Claim _claim;
+
+        public ClaimFilter(Claim claim)
+        {
+            _claim = claim;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if(!context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                context.Result = new StatusCodeResult(401);
+                return;
+            }
+
+            if(!CustomAuthorization.UserClaimsValid(context.HttpContext, _claim.Type, _claim.Value))
+            {
+                context.Result = new StatusCodeResult(403);
+            }
+        }
+    }
+}
