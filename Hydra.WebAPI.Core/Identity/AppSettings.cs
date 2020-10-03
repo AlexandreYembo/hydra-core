5bf863fbbb883f86912c49d8f38b638755b4bf3c
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-10-03 11:45:02 +0100

    Update AppSettings.cs

--- Hydra.WebAPI.Core/Identity/AppSettings.cs
+++ Hydra.WebAPI.Core/Identity/AppSettings.cs
@@ -4,7 +4,7 @@ namespace Hydra.WebAPI.Core.Identity
     {
         
         public string Secret { get; set; }
-        public int ExpirationTime { get; set; }
+        public double ExpirationTime { get; set; }
         public string ValidAudience { get; set; }
         public string ValidIssuer { get; set; }
     }
