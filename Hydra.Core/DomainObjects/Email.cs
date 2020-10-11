a95f71edf8e8399629eddee94708776daa2f188f
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-10-11 10:20:09 +0100

    Update Email.cs

--- Hydra.Core/DomainObjects/Email.cs
+++ Hydra.Core/DomainObjects/Email.cs
@@ -14,7 +14,7 @@ namespace Hydra.Core.DomainObjects
             Value = email;
         }
 
-        public bool IsValid(string email){
+        public static bool IsValid(string email){
             var emailRegex = new Regex(@"^(?("")("".+?""@)|(([0-9a-zA-Z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-zA-Z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,6}))$");
             return emailRegex.IsMatch(email);
         }
