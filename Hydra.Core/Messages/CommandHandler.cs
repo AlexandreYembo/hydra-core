ffc431c2bf083f097b8dccf87ac576dd394a0b5e
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-10-11 10:15:58 +0100

    add email to domain objects

--- Hydra.Core/Messages/CommandHandler.cs
+++ Hydra.Core/Messages/CommandHandler.cs
@@ -16,9 +16,15 @@ namespace Hydra.Core.Messages
         protected void AddError(string message) => 
             ValidationResult.Errors.Add(new ValidationFailure(string.Empty, message));
 
+
+        /// <summary>
+        /// Persist the data on DB and also valid for errors.
+        /// </summary>
+        /// <param name="uow"></param>
+        /// <returns></returns>
         protected async Task<ValidationResult> Save(IUnitOfWork uow)
         {
-                if(await uow.Commit()) AddError("Error to save the customer");
+            if(await uow.Commit()) AddError("Error to save the customer");
 
             return ValidationResult;
         }
