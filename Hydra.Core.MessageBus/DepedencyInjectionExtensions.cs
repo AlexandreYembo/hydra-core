5d65e8a09389457c4dc0dd96527ae3a04053e082
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-10-12 22:50:33 +0100

    Update DepedencyInjectionExtensions.cs

--- Hydra.Core.MessageBus/DepedencyInjectionExtensions.cs
+++ Hydra.Core.MessageBus/DepedencyInjectionExtensions.cs
@@ -5,11 +5,12 @@ namespace Hydra.Core.MessageBus
 {
     public static class DepedencyInjectionExtensions
     {
-        public static void AddMessageBus(this IServiceCollection services, string connection)
+        public static IServiceCollection AddMessageBus(this IServiceCollection services, string connection)
         {
             if(string.IsNullOrEmpty(connection)) throw new ArgumentNullException();
 
             services.AddSingleton<IMessageBus>(new MessageBus(connection));
+            return services;
         }
     }
 } 
