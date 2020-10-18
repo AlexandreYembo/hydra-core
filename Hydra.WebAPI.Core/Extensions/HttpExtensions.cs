1227f7857dc24966d3d4c4251d5342a7d956646c
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-10-18 20:15:22 +0100

    Create HttpExtensions.cs

---
+++ Hydra.WebAPI.Core/Extensions/HttpExtensions.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hydra.WebAPI.Core.Extensions
+{
+    /// <summary>
+    /// Class used to validate the SSL Certificate
+    /// </summary>
+    public static class HttpExtensions
+    {
+        public static IHttpClientBuilder AllowSelfSignedCertificate(this IHttpClientBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            return builder.ConfigureHttpMessageHandlerBuilder(b =>
+            {
+                b.PrimaryHandler = 
+                    new HttpClientHandler { ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator };
+            });
+        }
+    }
+}
