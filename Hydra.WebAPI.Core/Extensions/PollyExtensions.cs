95e71de55f36a88fb658919a88684fabfab2d092
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-10-18 18:19:59 +0100

    Creating Polly extensions

---
+++ Hydra.WebAPI.Core/Extensions/PollyExtensions.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Net.Http;
+using Polly;
+using Polly.Extensions.Http;
+using Polly.Retry;
+
+namespace Hydra.WebAPI.Core.Extensions
+{
+    public static class PollyExtensions
+    {
+        public static AsyncRetryPolicy<HttpResponseMessage> WaitAndRetry()
+        {
+            var retry = HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(new[]
+                {
+                    TimeSpan.FromSeconds(1),
+                    TimeSpan.FromSeconds(5),
+                    TimeSpan.FromSeconds(10)
+                });
+
+            return retry;
+        }
+    }
+}
