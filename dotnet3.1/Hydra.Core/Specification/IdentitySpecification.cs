6982356d2391ebb15b93bd73c06e7ebe8b0b6a64
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-10-19 10:19:51 +0100

    moved to dotnet3.1 folder and adding specification pattern

---
+++ dotnet3.1/Hydra.Core/Specification/IdentitySpecification.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq.Expressions;
+
+namespace Hydra.Core.Specification
+{
+    internal sealed class IdentitySpecification<T> : Specification<T>
+    {
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            return x => true;
+        }
+    }
+}
