668a66fd7e193a1f7c31756d6602f01afe616101
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-10-23 17:41:21 +0100

    Update VoucherResponseMessage.cs

--- dotnet3.1/Hydra.Core.Integration/Messages/VoucherMessages/VoucherResponseMessage.cs
+++ dotnet3.1/Hydra.Core.Integration/Messages/VoucherMessages/VoucherResponseMessage.cs
@@ -6,7 +6,7 @@ namespace Hydra.Core.Integration.Messages.VoucherMessages
     public class VoucherResponseMessage : ResponseMessage
     {
         public VoucherResponseMessage(string code, decimal? discount,
-            VoucherType voucherType,  DateTime expirationDate, bool active, bool isUsed, ValidationResult validationResult) 
+            int voucherType,  DateTime expirationDate, bool active, bool isUsed, ValidationResult validationResult) 
         : base(validationResult)
         {
             Code = code;
@@ -19,15 +19,9 @@ namespace Hydra.Core.Integration.Messages.VoucherMessages
 
         public string Code { get; private set; }
         public decimal? Discount { get; private set; }
-        public VoucherType VoucherType { get; private set; }
+        public int VoucherType { get; private set; }
         public DateTime ExpirationDate { get; private set; }
         public bool Active { get; private set; }
         public bool IsUsed { get; private set; }
-    }
-
-     public enum VoucherType
-    {
-        Percentage = 0,
-        Value = 1
     }
 }
