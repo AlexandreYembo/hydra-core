f4b574ee225a82cdb76f0db4ec485b4220e74a23
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-10-31 16:11:30 +0000

    Create OrderInProcessingIntegrationEvent.cs

---
+++ dotnet3.1/Hydra.Core.Integration/Messages/OrderMessages/OrderInProcessingIntegrationEvent.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Hydra.Core.Integration.Messages.OrderMessages
+{
+    public class OrderInProcessingIntegrationEvent : IntegrationEvent
+    {
+        public Guid CustomerId { get; set; }
+        public Guid OrderId { get; set; }
+        public int PaymentType { get; set; }
+        public decimal Price { get; set; }
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string Expiration { get; set; }
+        public string CVV { get; set; }
+    }
+}
