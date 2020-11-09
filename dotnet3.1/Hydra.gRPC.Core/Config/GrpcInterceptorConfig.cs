5e33fbd804504c14dc3cd5c7d8c8c653e033bf50
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-11-09 07:57:57 +0000

    Update GrpcInterceptorConfig.cs

--- dotnet3.1/Hydra.gRPC.Core/Config/GrpcInterceptorConfig.cs
+++ dotnet3.1/Hydra.gRPC.Core/Config/GrpcInterceptorConfig.cs
@@ -10,10 +10,21 @@ namespace Hydra.gRPC.Core.Config
         /// </summary>
         /// <param name="builder"></param>
         /// <returns></returns>
-        public static IHttpClientBuilder AddgRPCInterceptor(this IHttpClientBuilder builder)
+        public static IHttpClientBuilder AddgGRPCInterceptor(this IHttpClientBuilder builder)
         {
             builder.AddInterceptor<GRPCServiceInterceptor>();
             return builder;
+        }
+
+         /// <summary>
+        /// Register interceptors for gRPC
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static void RegisterInterceptor(this IServiceCollection services)
+        {
+            services.AddSingleton<GRPCServiceInterceptor>();
+
         }
     }
 }
